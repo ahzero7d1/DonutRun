8bcae8c5654b6da619700c7f2a1fb01349e2aff5
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-12 02:51:58 +0900

    obstacle script
    Making obstacle up/down, left/right script

---
+++ Assets/Obstacle_Scripts/Obstacle_left_and_right.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_left_and_right : MonoBehaviour
+{
+    public float speed = 3f;
+    bool switc = true;
+    private SpriteRenderer spriteRenderer;
+    Vector2 pos;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (switc)
+        {
+            movebirdup();
+        }
+        else
+        {
+            movebirddown();
+        }
+
+        if (transform.position.y >= pos.y + 2)
+        {
+            switc = false;
+            spriteRenderer.flipX = true;
+        }
+        else if (transform.position.y <= pos.y - 1)
+        {
+            switc = true;
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void movebirdup()
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+
+    void movebirddown()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Obstacle_Scripts/Obstacle_up_and_down.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_up_and_down : MonoBehaviour
+{
+    public float speed = 1f;
+    bool switc = true;
+    private SpriteRenderer spriteRenderer;
+    Vector2 pos;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (switc)
+        {
+            movebirdright();
+        }
+        else
+        {
+            movebirdleft();
+        }
+
+        if (transform.position.x >= pos.x + 2)
+        {
+            switc = false;
+            spriteRenderer.flipX = true;
+        }
+        else if (transform.position.x <= pos.x - 2)
+        {
+            switc = true;
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void movebirdright()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    void movebirdleft()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+}
