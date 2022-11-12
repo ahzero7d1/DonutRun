62df9071a78c77da965945eec6ea3d9d473afb9f
Karen <mingki080808@gmail.com>
2022-11-12 09:13:03 +0900

    playermove_basic

---
+++ Assets/Player_Script/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    public int playerSpeed;
+
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(playerSpeed,rigid.velocity.y);
+    }
+}
