e49803f42eae5b597f4d24aa2c101eb89208eb37
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-12 17:42:09 +0900

    frying pan update

--- Assets/script/Obstacle_Scripts/Obstacle_fryingpan.cs
+++ Assets/script/Obstacle_Scripts/Obstacle_fryingpan.cs
@@ -19,7 +19,7 @@ public class Obstacle_fryingpan : MonoBehaviour
     {
         ImpulseForce();
         MoveToPlayer1();
-        Invoke("Deactive", 3);
+        Invoke("Deactive", 1.5);
         //Invoke("Active", 3);
     }
 
