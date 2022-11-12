f58732cc809eab7774ddc6d86c03932c1a40fa4b
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-12 11:46:06 +0900

    frypan 업데이트

--- Assets/script/Obstacle_Scripts/Obstacle_fryingpan.cs
+++ Assets/script/Obstacle_Scripts/Obstacle_fryingpan.cs
@@ -6,30 +6,19 @@ public class Obstacle_fryingpan : MonoBehaviour
 {
     public float speed = 3f;
     Rigidbody2D rigid;
-    Vector2 playerPos;
-    Vector3 target;
-    Vector3 player_target;
+    Vector2 startPos;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
-        playerPos = GameObject.Find("Player").transform.position;
-        player_target = new Vector3(playerPos.x, playerPos.y, 0);
-        target = new Vector3(playerPos.x-5, playerPos.y, 0);
+        startPos = transform.position;
     }
 
     void FixedUpdate()
     {
         MoveToPlayer1();
-    }
-
-    void ONCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Floor")
-        {
-            Invoke("Deactive", 3);
-        }
+        Invoke("Deactive", 1);
+        //Invoke("Active", 3);
     }
 
     void Deactive()
@@ -37,9 +26,18 @@ public class Obstacle_fryingpan : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    void Active()
+    {
+        gameObject.SetActive(true);
+    }
+
     void MoveToPlayer1()
     {
-        rigid.AddForce(Vector2.right*0.2f, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * 0.2f, ForceMode2D.Impulse);
+    }
 
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startPos, 5);
     }
 }
