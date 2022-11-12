2d2ba0beca3d2d688e5f193f660f81450a4939e4
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-12 12:45:25 +0900

    프라이팬 수정

---
+++ Assets/script/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject frypan;
+    int second;
+
+    void FixedUpdate()
+    {
+        second = Random.Range(3, 7);
+        Invoke("SpawnPlay", second);
+    }
+
+    void SpawnPlay()
+    {
+        Instantiate(frypan, transform.position);
+    }
+}
