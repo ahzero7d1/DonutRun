ed75fc7fec8b567782d4f47fba694ec344788961
Karen <mingki080808@gmail.com>
2022-11-12 13:14:25 +0900

    doublejump_finish

--- Assets/script/Player_Script/PlayerMove.cs
+++ Assets/script/Player_Script/PlayerMove.cs
@@ -10,6 +10,7 @@ public class PlayerMove : MonoBehaviour
     public gameManager GameManager;
     public float jumpPower;
     public bool doublejump;
+    public float defaultRay;
 
 
 
@@ -33,14 +34,16 @@ public class PlayerMove : MonoBehaviour
         //이단점프까지만 되도록 하기
         if(Input.GetButtonDown("Jump")){//doublejump가 false인 경우 1단점프
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position,Vector3.down,1,LayerMask.GetMask("platform"));
-            Debug.Log(rayHit.distance);
+            
 
-            if(rayHit.distance < 0.7f&& rayHit.distance >0.4f){
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position,Vector3.down,1,LayerMask.GetMask("platform"));
+
+
+            if(rayHit.distance > 0.4f && rayHit.distance < 0.7f ){//jumpPower 고려해서 바꾸기
                 rigid.AddForce(Vector2.up*jumpPower,ForceMode2D.Impulse);
                 doublejump = true;}
 
-            else if((rayHit.distance == 0f || rayHit.distance > 0.7f) && doublejump ){//doublejump가 true인 경우 2단 점프
+            else if((rayHit.distance == 0f || rayHit.distance > 0.7f ) && doublejump ){//doublejump가 true인 경우 2단 점프
                 rigid.AddForce(Vector2.up*jumpPower,ForceMode2D.Impulse);
                 doublejump = false;
             }
