3f7ae3ec20692f8941e4bf9f2b7ed23699e053d1
Karen <mingki080808@gmail.com>
2022-11-13 13:19:17 +0900

    distance측정가능

---
+++ Assets/script/Player_Script/Distance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour
+{
+    public Transform sucessZone;
+    public float distance;
+
+    public void Update(){
+        distance = (sucessZone.transform.position.x - transform.position.x);
+        Debug.Log(distance);
+
+        if(distance <= 0){
+            Debug.Log("Finish");
+        }
+    }
+
+}
