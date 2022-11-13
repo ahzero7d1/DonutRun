4fd4c802adf66813a2a64971d24a0bd2bfcc570d
정은수 <80330331+intsoo@users.noreply.github.com>
2022-11-13 14:50:24 +0900

    Button click sound

---
+++ Assets/BtnClickSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnClickSound : MonoBehaviour
+{
+    // sound source component
+    private AudioSource btnAudio;
+    public AudioClip btnClickClip;  // 장애물과 충돌 시 도넛 잃는 소리
+
+    void Awake()
+    {
+        // sound
+        btnAudio = GetComponent<AudioSource>();  // 점프 소리가 기본
+    }
+
+    public void playSound(){
+        // 버튼클릭소리
+        btnAudio.clip = btnClickClip;
+        btnAudio.Play();  // 사운드 재생
+        Debug.Log("버튼 클릭");
+    }
+}
