d093392bdbcca2b030a2ca55d2abcffc9659def9
Karen <mingki080808@gmail.com>
2022-11-13 10:57:03 +0900

    d

---
+++ Assets/Scenes_add/ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeSceneBtn()
+    {
+        switch (this.gameObject.name)
+        {
+            case "StoryButton":
+                SceneManager.LoadScene("StoryScene");
+                break;
+            case "StoryButton2":
+                SceneManager.LoadScene("StoryScene2");
+                break;
+            case "MainButton":
+                SceneManager.LoadScene("SampleScene");
+                break;
+        }
+
+
+    }
+
+
+
+}
