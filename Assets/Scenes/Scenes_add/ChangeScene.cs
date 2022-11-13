d07c10ac8d42d69f870e1ceb5690600d6fa53233
정은수 <80330331+intsoo@users.noreply.github.com>
2022-11-13 13:12:27 +0900

    Add effect sound when button is clicked
    StartScene에서 버튼 클릭 시 효과음 넣기, 프롤로그 BGM 추가

--- Assets/Scenes/Scenes_add/ChangeScene.cs
+++ Assets/Scenes/Scenes_add/ChangeScene.cs
@@ -8,11 +8,25 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
 
+    // sound source component
+    private AudioSource playerAudio;
+    // sound clip
+    public AudioClip btnClickClip;  // 장애물과 충돌 시 도넛 잃는 소리
+    
+    void Awake() {
+        // sound
+        playerAudio = GetComponent<AudioSource>();  // 점프 소리가 기본
+    } 
+
     public void ChangeSceneBtn()
     {
         switch (this.gameObject.name)
         {
             case "StoryButton":
+                //button click sound
+                playerAudio.Play();  // 재생
+                Debug.Log("클릭됨");    
+
                 SceneManager.LoadScene("StoryScene");
                 break;
         }
