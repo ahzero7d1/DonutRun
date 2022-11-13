8a3d8cf64d625291c128e78cdb4ae30a8d968da4
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-13 14:44:22 +0900

    Success Scene number create

--- Assets/Success_Text.cs
+++ Assets/Success_Text.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Success_Screen : MonoBehaviour
+public class Success_Text : MonoBehaviour
 {
     public void Setup() {
+        //gameObject.text = ("도넛 "+GameManager.donutPoint+" 획득!!").ToString();
         gameObject.SetActive(true);
     }
 }
